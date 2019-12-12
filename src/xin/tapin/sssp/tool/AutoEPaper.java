c3e09b2e79e69dff5f2a02c8fb2434b442daf1ec
YWQ <YWQ@SC-201901071514>
2019-12-12 17:34:04 +0800

    更新AutoEPaper.java

--- src/xin/tapin/sssp/tool/AutoEPaper.java
+++ src/xin/tapin/sssp/tool/AutoEPaper.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class AutoEPaper {
 	
 //	public static void main(String[] args) {
-//		ArrayList<Integer> arrayList = getArrayList(30, 2, 10);
+//		ArrayList<Integer> arrayList = getArrayList(34, 12, 2);
 //		for (Integer integer : arrayList) {
 //			System.out.println(integer);
 //		}
